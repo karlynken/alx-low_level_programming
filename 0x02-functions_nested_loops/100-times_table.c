c39e8aad2c06e2d0a0e97c2a50cbc51b769e323c
karlynken <karlynken@gmail.com>
2023-02-22 13:23:00 -0800

    balances

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,8 +32,8 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-                                }else if (k >= 100)
+					_putchar((k % 10) + '0'); 
+			        } else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -43,7 +43,6 @@ void print_times_table(int n)
 				}
 			}
 			_putchar('\n');
-
 	         }
 	}
 }
