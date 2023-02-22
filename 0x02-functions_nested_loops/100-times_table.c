cd2803d56cda9757eab30fea692680eb0f5c8063
karlynken <karlynken@gmail.com>
2023-02-22 13:06:40 -0800

    balances

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
+
 void print_times_table(int n)
 {
-       int i, j, k;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -29,9 +31,9 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					i_putchar((k / 10) + '0');
+					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-                                } else if (k >= 100)
+                                }else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -41,6 +43,7 @@ void print_times_table(int n)
 				}
 			}
 			_putchar('\n');
-		}
+
+			}
 	}
 }
