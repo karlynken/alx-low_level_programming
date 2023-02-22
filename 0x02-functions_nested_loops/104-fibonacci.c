d9317032b888870440aef87a38f53fd0f0a281d3
karlynken <karlynken@gmail.com>
2023-02-22 15:30:08 -0800

    assignment

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - fibonacci <3
  *
@@ -15,7 +14,7 @@ int main(void)
 	unsigned long int bef = 1;
 	unsigned long int aft = 2;
 	unsigned long int l = 1000000000;
-        unsigned long int bef1;
+	unsigned long int bef1;
 	unsigned long int bef2;
 	unsigned long int aft1;
 	unsigned long int aft2;
