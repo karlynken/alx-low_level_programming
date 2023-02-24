2a8791aafecb2679f67200a5f3778f71f8a6ec38
karlynken <karlynken@gmail.com>
2023-02-23 21:23:56 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* print_numbers - prints numbers between 0 to 9.
-* Return: no return.
-*/
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
 void print_numbers(void)
 {
 	int ch;
