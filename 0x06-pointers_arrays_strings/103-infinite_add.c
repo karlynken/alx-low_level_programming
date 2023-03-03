91f0880d0ebfbec70a58109868a35d8f192f1c79
karlynken <karlynken@gmail.com>
2023-03-03 13:27:20 -0800

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,7 +14,7 @@ void rev_string(char *n)
 
 	while (*(n + i) != '\0')
 	{
-		i++;
+	      i++;
 	}
 	i--;
 
@@ -42,7 +42,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (*(n1 + i) != '\0')
 		i++;
-        while (*(n2 + j) != '\0')
+	while (*(n2 + j) != '\0')
 		j++;
 	i--;
 	j--;
