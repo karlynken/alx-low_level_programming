f9fb893607ab16bfe45f882bcc38a74de20ca8f4
karlynken <karlynken@gmail.com>
2023-04-05 19:24:19 -0700

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,20 +3,25 @@
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the list_t list.
+ * @h: head of the list.
  *
- * Return: The number of nodes in the list_t list.
+ * Return: The number of nodes in the list.
  */
 size_t print_listint(const listint_t *h)
-{
-	size_t nodes = 0;
 
-	while (h)
+
+
+
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
 	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", cursor->n);
+		count += 1;
+		cursor = cursor->next;
 	}
 
-	return (nodes);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
 
 #include <stdlib.h>
@@ -9,7 +9,7 @@
  * @n: integer
  * @next: points to the next node
  * Description: singly linked list node structure
- * for project
+ *
  */
 
 typedef struct listint_s
@@ -17,6 +17,18 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
